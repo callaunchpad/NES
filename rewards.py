accc3d4fa53f555641795467a3e5cf3caa921327
Felix Su <felixs8696@berkeley.edu>
2017-09-27 07:40:42 -0700

    docstrings for rewards

--- rewards.py
+++ rewards.py
@@ -7,4 +7,12 @@ def resolve_reward(name):
 	return rewards[name]
 
 def manhattan_distance(current, target):
+	"""
+	Manhattan distance from current position to target
+	Args:
+	    current (tuple): x, y coordinates of the current position
+	    target (tuple): x, y coordinates of the target
+	Returns:
+		(float): Manhattan distance from current position to target
+	"""
 	return -(abs(target[0] - current[0]) + abs(target[1] - current[1]))
