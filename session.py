3e92b21c2f9b52d6693cf8a525efe62709ae438e
Felix Su <felixs8696@berkeley.edu>
2017-09-27 07:06:57 -0700

    set random seed for duplicability

--- session.py
+++ session.py
@@ -1,5 +1,9 @@
+import numpy as np
+import tensorflow as tf
 from algorithm import NES
 
 if __name__ == "__main__":
+	np.random.seed(0)
+	tf.set_random_seed(0)
     algorithm = NES()
     algorithm.run()
