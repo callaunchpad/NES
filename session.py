7f8be64fe3117ca497b1a8fac92521fd996c50c9
Felix Su <felixs8696@berkeley.edu>
2017-09-28 18:04:19 -0700

    changed noise std dev to improve convergence

--- session.py
+++ session.py
@@ -8,25 +8,34 @@ from shutil import copyfile, rmtree
 from datetime import datetime
 from algorithm import NES
 
-if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description="Flags to toggle options")
-	parser.add_argument('-d', action='store_true', default=False, help='Debug flag, Delete log after training is finished.')
-	args = parser.parse_args()
-
+def set_seeds(seed):
 	np.random.seed(0)
 	tf.set_random_seed(0)
+
+def get_args():
+	parser = argparse.ArgumentParser(description="Flags to toggle options")
+	parser.add_argument('-d', action='store_true', default=False, help='Debug flag, Delete log after training is finished.')
+	return parser.parse_args()
+
+def create_training_contents():
 	timestamp = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
 	try:
 		os.makedirs("ext/" + timestamp)
 	except OSError as e:
-		if e.errno != errno.EEXIST:
-			raise
+		if e.errno != errno.EEXIST: raise
 	training_directory = "ext/" + timestamp + "/"
 	log_file = training_directory + timestamp + '.log'
+	return training_directory, log_file, timestamp
+
+if __name__ == "__main__":
+	args = get_args()
+
+	training_directory, log_file, timestamp = create_training_contents()
 	logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(message)s')
 	copyfile("Config.yaml", training_directory + timestamp + ".yaml")
 
 	try:
+		set_seeds(0)
 		algorithm = NES(training_directory)
 		print("Running NES Algorithm...")
 		print("Check {} for progress".format(log_file))
